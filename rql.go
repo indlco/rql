612478a75d6334922bb60cdebc673db16beaed68
nxso <73075979+nxso@users.noreply.github.com>
2023-08-21 15:33:44 +0200

    log message linebreak added

--- rql.go
+++ rql.go
@@ -424,14 +424,14 @@ func (p *Parser) parseField(sf reflect.StructField) error {
 			filterOps = append(filterOps, EQ, NEQ, IN, LT, LTE, GT, GTE)
 		default:
 			if !v.Type().ConvertibleTo(reflect.TypeOf(time.Time{})) {
-				fmt.Printf("rql: field type for %q is not supported - only allowed for select", sf.Name)
+				fmt.Printf("rql: field type for %q is not supported - only allowed for select\n", sf.Name)
 			}
 			f.ValidateFn = validateTime(layout)
 			f.CovertFn = convertTime(layout)
 			filterOps = append(filterOps, EQ, NEQ, IN, LT, LTE, GT, GTE)
 		}
 	default:
-		fmt.Printf("rql: field type for %q is not supported - only allowed for select", sf.Name)
+		fmt.Printf("rql: field type for %q is not supported - only allowed for select\n", sf.Name)
 	}
 	for _, op := range filterOps {
 		f.FilterOps[p.op(op)] = true
